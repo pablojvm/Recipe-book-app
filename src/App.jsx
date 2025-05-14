b0a840437483774d84f98837d5e555cc7b8c4688
Gaelle <gaelle.madelaine@gmail.com>
2025-05-14 19:00:08 +0200

    day2

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import NavBar from "./NavBar";
 import "./App.css";
 import Footer from "./Footer";
 import SideBar from "./SideBar";
+import Recetas from "./Recetas";
 
 function App() {
   return (
     <div id="App">
       <NavBar />
-      <SideBar />
+      <div id="Sidebar-Recetas">
+        <SideBar />
+        <Recetas />
+      </div>
       <Footer />
     </div>
   );
